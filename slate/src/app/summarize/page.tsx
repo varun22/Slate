4b315db999ec971035bbe503c25426131db6cefe
aaryanmohta04 <64299769+aaryanmohta04@users.noreply.github.com>
2024-04-28 11:09:44 -0700

    design

--- slate/src/app/summarize/page.tsx
+++ slate/src/app/summarize/page.tsx
@@ -11,11 +11,11 @@ import { BsQuestionSquareFill } from "react-icons/bs";
 import { BsLightningCharge } from "react-icons/bs";
 import { PiChatsThin } from "react-icons/pi";
 
-const Card = ({ values }: any) => {
+const Card = () => {
   return (
     <a
       href="/classes"
-      className="block w-[410px] h-32 p-6 bg-white bg-opacity-0"
+      className="block w-[410px] h-32 p-4 bg-white bg-opacity-0"
     >
       <div className="flex flex-row">
         <div className=" text-5xl font-semibold  text-gray-900">ECS 161</div>
@@ -33,13 +33,15 @@ const Card = ({ values }: any) => {
 const Suggestions = ({ values, onClick, icon }: any) => {
   return (
     <button
-      className="block w-32 p-6 bg-white m-3 flex flex-wrap"
+      className="block w-64 p-6 bg-white m-3 flex flex-wrap rounded-xl shadow"
       onClick={onClick}
     >
       <div className="flex flex-row">
-        <div className="pl-1 pt-3">{icon}</div>
+        <div className=" py-2 text-3xl text-neutral-500">{icon}</div>
       </div>
-      <div className="text-xl font-bold text-gray-900 ">{values}</div>
+      <div className="text-l font-medium text-gray-900 text-left ">
+        {values}
+      </div>
     </button>
   );
 };
@@ -64,55 +66,61 @@ const SummarizePage: React.FC = () => {
   };
 
   return (
-    <div>
-      <div className="flex-row flex justify-center bg-grey grid grid-cols-5">
+    <div className="bg-grey">
+      <div className="justify-center bg-grey grid grid-cols-5">
         <div>
           <Sidebar />
         </div>
         <Card />
-        <div className="flex flex-row p-3 self-align-center">
-          <Suggestions
-            values="Get the latest organized class notes"
-            onClick={() => handleSuggestionClick("Get updated class notes")}
-            icon={<RxPencil2 />}
-          />
-          <Suggestions
-            values="Prepare for your next quiz with Questions & answers"
-            onClick={() =>
-              handleSuggestionClick(
-                "Provide questions & answers for topics : []"
-              )
-            }
-            icon={<BsQuestionSquareFill />}
-          />
-          <Suggestions
-            values="Test yourself with quick flash cards"
-            onClick={() => handleSuggestionClick("Test me with flash cards")}
-            icon={<BsLightningCharge />}
-          />
-          <Suggestions
-            values="Or type any questions to the AI or the forum"
-            onClick={() => handleSuggestionClick("[Type your Questions here!]")}
-            icon={<PiChatsThin />}
-          />
+      </div>
+      <div className="pb-4">
+        <div className="pl-64 text-center font-semibold text-neutral-500 text-4xl pt-28">
+          Get started by selecting one of
         </div>
-        <div className="flex h-screen mb-10 col-span-4 col-start-2">
-          <textarea
-            value={inputText}
-            placeholder="Ask the AI any question from the notes"
-            className="rounded-md self-end p-3 justify-self-center shadow-md"
-            onChange={handleInputChange}
-            rows={1}
-            cols={150}
-            style={{ color: "black" }} // Apply inline style for text color
-          />
-          <button
-            onClick={handleSummarize}
-            className="m-2 self-end justify-self-center"
-          >
-            <IoSend />
-          </button>
+        <div className="pl-64 text-center font-semibold text-neutral-500 text-4xl pt-2">
+          the following actions...
         </div>
+      </div>
+      <div className="pl-64 flex flex-row p-3 align-center justify-center">
+        <Suggestions
+          values="Get the latest organized class notes"
+          onClick={() => handleSuggestionClick("Get updated class notes")}
+          icon={<RxPencil2 />}
+        />
+        <Suggestions
+          values="Prepare for your next quiz with Questions & answers"
+          onClick={() =>
+            handleSuggestionClick("Provide questions & answers for topics : []")
+          }
+          icon={<BsQuestionSquareFill />}
+        />
+        <Suggestions
+          values="Test yourself with quick flash cards"
+          onClick={() => handleSuggestionClick("Test me with flash cards")}
+          icon={<BsLightningCharge />}
+        />
+        <Suggestions
+          values="Or type any questions to the AI or the forum"
+          onClick={() => handleSuggestionClick("[Type your Questions here!]")}
+          icon={<PiChatsThin />}
+        />
+      </div>
+      <div className="flex h-screen mb-10 col-span-4 col-start-2">
+        <textarea
+          value={inputText}
+          placeholder="Ask the AI any question from the notes"
+          className="rounded-md self-end p-3 justify-self-center shadow-md"
+          onChange={handleInputChange}
+          rows={1}
+          cols={150}
+          style={{ color: "black" }} // Apply inline style for text color
+        />
+        <button
+          onClick={handleSummarize}
+          className="m-2 self-end justify-self-center"
+        >
+          <IoSend />
+        </button>
       </div>
     </div>
   );
