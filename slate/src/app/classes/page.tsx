2dddb505d232b766cde6a295b171fd65d3fc2af8
varun22 <84626068+varun22@users.noreply.github.com>
2024-04-28 07:04:08 -0700

    worked on upload page

--- slate/src/app/classes/page.tsx
+++ slate/src/app/classes/page.tsx
@@ -2,12 +2,38 @@
 import "firebase/compat/auth";
 import FileUpload from "@/components/file-upload";
 import Sidebar from "@/components/sidebar";
+import Image from "next/image";
+import arrow from "../../../public/arrow.svg";
+
+const Card = ({ values }: any) => {
+  return (
+    <a
+      href="/classes"
+      className="block w-[410px] h-32 p-6 bg-white bg-opacity-0"
+    >
+      <div className="flex flex-row">
+        <div className=" text-5xl font-semibold  text-gray-900">ECS 161</div>
+        <div className="pl-1 pt-3">
+          <Image alt="" src={arrow}></Image>
+        </div>
+      </div>
+      <div className="text-xl font-bold  text-gray-900 ">
+        University of California, Davis
+      </div>
+    </a>
+  );
+};
 
 const classPage: React.FC = () => {
   return (
-    <div>
-      <Sidebar />
-      <FileUpload />
+    <div className="grid grid-cols-5  bg-grey">
+      <div>
+        <Sidebar />
+      </div>
+      <div className="col-span-4 m-10 h-screen">
+        <Card />
+        <FileUpload />
+      </div>
     </div>
   );
 };
